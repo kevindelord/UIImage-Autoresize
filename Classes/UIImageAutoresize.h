8330b6f5b6671622c1828840491e4084595c1779
kevindelord <delord.kevin@gmail.com>
2016-04-26 17:29:44 +0200

    Create new plist files and umbrella header

---
+++ Classes/UIImageAutoresize.h
@@ -0,0 +1,13 @@
+//
+//  UIImageAutoresize.h
+//  UIImage+Autoresize
+//
+//  Created by kevin delord on 26/04/16.
+//  Copyright © 2016 Smart Mobile Factory. All rights reserved.
+//
+
+#ifndef UIImageAutoresize_h
+#define UIImageAutoresize_h
+
+
+#endif /* UIImageAutoresize_h */
