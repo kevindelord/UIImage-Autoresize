335797c073774a15ca27e8256360f5824a5b9f19
kevindelord <delord.kevin@gmail.com>
2016-04-28 10:49:50 +0200

    Fix unit test for image name

--- UIImage+AutoresizeTests/UIImage_MainBundleTests.m
+++ UIImage+AutoresizeTests/UIImage_MainBundleTests.m
@@ -7,11 +7,11 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "UIImage+Autoresize.h"
 
 @interface UIImage_MainBundleTests : XCTestCase
 
 @end
-
 
 @implementation UIImage_MainBundleTests
 
@@ -22,6 +22,13 @@
 	UIImage *imageinBundle = [UIImage imageNamed:@"test.bg@2x.png" inBundle:[NSBundle mainBundle] compatibleWithTraitCollection:nil];
 	XCTAssert(imageinBundle != nil);
 	XCTAssertNotNil(imageinBundle);
+}
+
+- (void)testShouldReturnNilWhenUsingMainBundleOnTestTarget {
+	UIImage *imageTestBundle = [UIImage imageNamed:@"test.bg.png" withTransitionSize:CGSizeMake(320.0f, 480.0f) inBundle:[NSBundle mainBundle]];
+	XCTAssertNil(imageTestBundle);
+	UIImage *imageMainBundle = [UIImage imageNamed:@"test.bg.png" withTransitionSize:CGSizeMake(568.0f, 320.0f) inBundle:[NSBundle mainBundle]];
+	XCTAssertNil(imageMainBundle);
 }
 
 @end
